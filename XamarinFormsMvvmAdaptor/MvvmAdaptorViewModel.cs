e0bd424e9ed505530fe161009b1ee4b20995a74d
Guy Antoine <guy@zeebz.net>
2020-02-08 18:31:47 +0200

    Add BaseViewModel

---
+++ XamarinFormsMvvmAdaptor/MvvmAdaptorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MvvmHelpers;
+
+namespace XamarinFormsMvvmAdaptor
+{
+    public class MvvmAdaptorViewModel : BaseViewModel, IMvvmAdaptorViewModel
+    {
+        public Task InitializeAsync(object navigationData)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
