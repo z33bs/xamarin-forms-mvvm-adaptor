f8f53ddc27da8615a42a7af954822f21e2ff523a
Guy Antoine <guy@zeebz.net>
2020-02-26 20:37:46 +0200

    Clean unused exceptions

--- XamarinFormsMvvmAdaptor/Exceptions.cs
+++ XamarinFormsMvvmAdaptor/Exceptions.cs
@@ -5,30 +5,10 @@ namespace XamarinFormsMvvmAdaptor
     public class NotInitializedException : Exception
     {
         /// <summary>The <see cref="NavController"/> is not initialized</summary>
-        public NotInitializedException()
-        { }
-        /// <summary>The <see cref="NavController"/> is not initialized</summary>
         public NotInitializedException(string message) : base(message)
         { }
         /// <summary>The <see cref="NavController"/> is not initialized</summary>
         public NotInitializedException(string message, Exception innerException) : base(message, innerException)
         { }
-    }
-
-    /// <summary>The <see cref="Xamarin.Forms.Page"/>'s BindingContext is not set</summary>
-    public class BindingContextNotSetException : Exception
-    {
-        /// <summary>The <see cref="Xamarin.Forms.Page"/>'s BindingContext is not set</summary>
-        public BindingContextNotSetException()
-        { }
-    }
-
-    /// <summary>The ViewModel does not implement <see cref="IAdaptorViewModel"/></summary>
-    public class NotIAdaptorViewModelException : Exception
-    {
-        /// <summary>The ViewModel does not implement <see cref="IAdaptorViewModel"/></summary>
-        public NotIAdaptorViewModelException()
-        { }
-    }
-    
+    }    
 }
