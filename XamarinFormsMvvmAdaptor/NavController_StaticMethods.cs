d5af556535f3c6d3871530b20da5ab0ccdfff514
Guy Antoine <guy@zeebz.net>
2020-02-26 08:13:06 +0200

    Make GetPageTypeForViewModel even more robust
    Suffix only replaces last occurance of word

--- XamarinFormsMvvmAdaptor/NavController_StaticMethods.cs
+++ XamarinFormsMvvmAdaptor/NavController_StaticMethods.cs
@@ -31,8 +31,8 @@ namespace XamarinFormsMvvmAdaptor
         {
             var nameSpace = viewModelType.Namespace
                             .Replace(_viewModelSubNamespace, _viewSubNamespace);
-            var name = viewModelType.Name
-                            .Replace(_viewModelSuffix, _viewSuffix);
+            var name = ReplaceLastOccurrence(
+                            viewModelType.Name, _viewModelSuffix, _viewSuffix);
 
             var viewAssemblyName = string.Format(CultureInfo.InvariantCulture
                 , "{0}.{1}, {2}"
@@ -41,6 +41,16 @@ namespace XamarinFormsMvvmAdaptor
                 , viewModelType.GetTypeInfo().Assembly.FullName);
 
             return Type.GetType(viewAssemblyName);
+        }
+
+        private static string ReplaceLastOccurrence(string source, string find, string replace)
+        {
+            int place = source.LastIndexOf(find);
+
+            if (place == -1)
+                return source;
+
+            return source.Remove(place, find.Length).Insert(place, replace);
         }
 
         private static void BindViewModelToPage(Page page, IAdaptorViewModel viewModel)
