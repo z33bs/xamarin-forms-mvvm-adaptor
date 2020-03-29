a3037ff140129333668f1e18298ce4f07f5051ad
Guy Antoine <guy@zeebz.net>
2020-03-29 13:23:29 +0200

    ReOrganise and Refactor

---
+++ XamarinFormsMvvmAdaptor/IMvvm_old.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsMvvmAdaptor
+{
+    /// <summary>
+    /// MvvM Navigation Controller. Each instance contains its own Navigation stack.
+    /// Navigation engine depends on the user sticking to a naming convention:
+    /// Views are expected to be in the <see cref="Mvvm.DEFAULT_V_NAMESPACE"/> subnamespace
+    /// and end with suffix <see cref="Mvvm.DEFAULT_V_SUFFIX"/>.
+    /// ViewModels are expected to be in the <see cref="Mvvm.DEFAULT_VM_NAMESPACE"/> subnamespace
+    /// and end with suffix <see cref="Mvvm.DEFAULT_VM_SUFFIX"/>.
+    /// This behaviour can be customised with <see cref="Mvvm.SetNamingConventions(string, string, string, string)"/>
+    /// </summary>
+    public interface IMvvmOld
+    {
+        #region Properties
+        /// <summary>
+        /// Returns true if InitAsync has run successfully
+        /// </summary>
+        bool IsInitialized { get; }
+
+        /// <summary>
+        /// Returns the <see cref="NavigationPage"/> which is the root of the <see cref="IMvvm"/>.
+        /// Set your <see cref="Application.MainPage"/> to this.
+        /// </summary>
+        Page NavigationRoot { get; }
+
+        /// <summary>
+        /// Gets the stack of pages in the navigation
+        /// </summary>
+        IReadOnlyList<Page> MainStack { get; }
+
+        /// <summary>
+        /// Gets the modal navigation stack. The <see cref="ModalStack"/> always hides the <see cref="MainStack"/>
+        /// </summary>
+        IReadOnlyList<Page> ModalStack { get; }
+
+        /// <summary>
+        /// Page at the root/bottom of the <see cref="MainStack"/>
+        /// </summary>
+        Page RootPage { get; }
+
+        /// <summary>
+        /// Currently visible <see cref="Page"/> at the top of the <see cref="MainStack"/>
+        /// or <see cref="ModalStack"/> if it has any <see cref="Page"/>s in it
+        /// </summary>
+        Page TopPage { get; }
+
+        /// <summary>
+        /// <see cref="Page"/> beneath the <see cref="TopPage"/>
+        /// It will be the page that appears when the currently visible
+        /// <see cref="Page"/> is Popped.
+        /// Returns null if <see cref="TopPage"/> is the <see cref="RootPage"/>
+        /// </summary>
+        Page HiddenPage { get; }
+
+        /// <summary>
+        /// Viewmodel corresponding to the <see cref="RootPage"/>
+        /// <exception><see cref="System.NullReferenceException"/> is thrown when the <see cref="RootPage"/>s
+        /// BindingContext has not been set.</exception>
+        /// </summary>
+        IMvvmViewModelBase RootViewModel { get; }
+
+        /// <summary>
+        /// Viewmodel corresponding to the <see cref="TopPage"/>
+        /// <exception><see cref="System.NullReferenceException"/> is thrown when the <see cref="TopPage"/>s
+        /// BindingContext has not been set.</exception>
+        /// </summary>
+        IMvvmViewModelBase TopViewModel { get; }
+
+        /// <summary>
+        /// Viewmodel corresponding to the <see cref="HiddenPage"/>
+        /// Returns null if <see cref="TopPage"/> is the <see cref="RootPage"/>
+        /// <exception><see cref="System.NullReferenceException"/> is thrown when the <see cref="HiddenPage"/>s
+        /// BindingContext has not been set.</exception>
+        /// </summary>
+        IMvvmViewModelBase HiddenViewModel { get; }
+        #endregion
+
+        #region Vanilla Implementation
+        /// <summary>
+        /// Set the <see cref="RootPage"/>, and initialize its ViewModel,
+        /// running the <see cref="IMvvmViewModelBase.OnViewPushedAsync(object)"/>
+        /// and <see cref="IMvvmViewModelBase.RefreshStateAsync"/> methods.
+        /// </summary>
+        /// <returns></returns>
+        Task InitAsync(Page rootPage, object initialisationData = null);
+
+        /// <summary>
+        /// Pushes a new page onto the stack given a <typeparamref name="TViewModel"/>.
+        /// </summary>
+        /// <typeparam name="TViewModel">ViewModel to Push</typeparam>
+        /// <param name="navigationData">Optional navigation data to be passed to the <see cref="MvvmViewModelBase.OnViewPushedAsync(object)"/>  method of the <typeparamref name="TViewModel"/></param>
+        /// <param name="animated">Option whether to animate the push or not</param>
+        /// <returns></returns>
+        Task PushAsync<TViewModel>(object navigationData = null, bool animated = true) where TViewModel : IMvvmViewModelBase;
+
+        /// <summary>
+        /// Pushes a new Modal page onto the navigation stack
+        /// </summary>
+        /// <typeparam name="TViewModel"></typeparam>
+        /// <param name="navigationData">Object to be passed to the <see cref="MvvmViewModelBase.OnViewPushedAsync(object)"/>  method</param>
+        /// <param name="animated">Animate the Push</param>
+        /// <returns></returns>
+        Task PushModalAsync<TViewModel>(object navigationData = null, bool animated = true) where TViewModel : IMvvmViewModelBase;
+
+        /// <summary>
+        /// Inserts a page in the navigation stack before
+        /// the page associated with the given ViewModel
+        /// </summary>
+        /// <typeparam name="TViewModelExisting"></typeparam>
+        /// <typeparam name="TViewModelNew"></typeparam>
+        /// <param name="navigationData">Optional navigation data that will be passed to
+        /// <see cref="MvvmViewModelBase.OnViewPushedAsync(object)"/></param>
+        /// <returns></returns>
+        Task InsertPageBefore<TViewModelExisting, TViewModelNew>(object navigationData = null) where TViewModelNew : IMvvmViewModelBase;
+        #endregion
+
+        #region Di Implementation
+        /// <inheritdoc cref="InitAsync(Page, object)"/>
+        Task DiInitAsync(IMvvmViewModelBase rootViewModel, object initialisationData = null);
+
+        /// <summary>
+        /// Pushes a new page onto the stack
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <param name="navigationData">Navigation data that will be passed to the
+        /// <see cref="MvvmViewModelBase.OnViewPushedAsync(object)"/> method</param>
+        /// <param name="animated"></param>
+        /// <returns></returns>
+        Task DiPushAsync(IMvvmViewModelBase viewModel, object navigationData = null, bool animated = true);
+
+        /// <summary>
+        /// Pushes a new modal page onto the modal stack
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <param name="navigationData">Navigation data that will be passed to the
+        /// <see cref="MvvmViewModelBase.OnViewPushedAsync(object)"/> method</param>
+        /// <param name="animated"></param>
+        /// <returns></returns>
+        Task DiPushModalAsync(IMvvmViewModelBase viewModel, object navigationData = null, bool animated = true);
+
+        /// <summary>
+        /// Inserts a page in the navigation stack before
+        /// the page associated with the given ViewModel
+        /// </summary>
+        /// <typeparam name="TViewModelExisting"></typeparam>
+        /// <param name="viewModel"></param>
+        /// <param name="navigationData">Optional navigation data that will be passed to the
+        /// <see cref="MvvmViewModelBase.OnViewPushedAsync(object)"/> method</param>
+        /// <returns></returns>
+        Task DiInsertPageBefore<TViewModelExisting>(IMvvmViewModelBase viewModel, object navigationData = null);
+
+        /// <summary>
+        /// Asynchronously removes the <see cref="TopPage" /> from the navigation stack, with optional animation.
+        /// </summary>
+        /// <param name="animated">Whether to animate the pop.</param>
+        Task<IMvvmViewModelBase> PopAsync(bool animated = true);
+
+        /// <summary>
+        /// Asynchronously dismisses the most recent modally presented <see cref="Page" />, with optional animation.
+        /// </summary>
+        /// <param name="animated">Whether to animate the pop.</param>
+        Task<IMvvmViewModelBase> PopModalAsync(bool animated = true);
+
+        /// <summary>
+        /// Pops the entire <see cref="MainStack"/>, leaving only the <see cref="RootPage"/>, with optional animation.
+        /// </summary>
+        /// <param name="animated">Whether to animate the pop.</param>
+        /// <returns></returns>
+        Task<IMvvmViewModelBase> PopToRootAsync(bool animated = true);
+
+        /// <summary>
+        /// Removes a <see cref="Page"/> from the <see cref="MainStack"/>
+        /// that corresponds to a given ViewModel
+        /// </summary>
+        /// <typeparam name="TViewModel"></typeparam>
+        Task RemovePageFor<TViewModel>() where TViewModel : IMvvmViewModelBase;
+        #endregion
+
+        #region Additional Shared Methods
+
+        /// <summary>
+        /// Removes all pages in the <see cref="MainStack"/> except for
+        /// the <see cref="TopPage"/>, which becomes the <see cref="RootPage"/> of the stack.
+        /// </summary>
+        Task<IMvvmViewModelBase> CollapseMainStack();
+
+        /// <summary>
+        /// Removes the <see cref="HiddenPage"/> from the <see cref="MainStack"/> but not the <see cref="ModalStack"/>
+        /// </summary>
+        /// <returns></returns>
+        Task RemovePreviousPageFromMainStack();
+        #endregion
+
+        #region CombinedStyle experimental
+        IIoc Ioc { get; }
+        Task<TViewModel> NewPushAsync<TViewModel>(object navigationData = null, bool animated = true) where TViewModel : class, IMvvmViewModelBase;
+        Task<TViewModel> NewPushModalAsync<TViewModel>(object navigationData = null, bool animated = true) where TViewModel : class, IMvvmViewModelBase;
+        Page Initialize<TViewModel>(bool mustWrapInNavigationPage = true) where TViewModel : class, IMvvmViewModelBase;
+
+        Task<TViewModel> NewPushAsync<TViewModel>(TViewModel viewModel, object navigationData = null, bool animated = true) where TViewModel : class, IMvvmViewModelBase;
+        Task<TViewModel> NewPushModalAsync<TViewModel>(TViewModel viewModel, object navigationData = null, bool animated = true) where TViewModel : class, IMvvmViewModelBase;
+
+        #endregion
+    }
+}
--- XamarinFormsMvvmAdaptor/Mvvm_SharedMethods.cs
+++ XamarinFormsMvvmAdaptor/Mvvm_SharedMethods.cs
@@ -4,13 +4,17 @@ using System.Threading.Tasks;
 using Page = Xamarin.Forms.Page;
 using Xamarin.Forms;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Globalization;
+using System.Reflection;
+//todo
+//Add config for mustTryInterfaceVariation
 namespace XamarinFormsMvvmAdaptor
 {
     ///<inheritdoc/>
     public partial class Mvvm : IMvvm
     {
-        #region Settings
+        #region SETTINGS
         const string DEFAULT_VM_NAMESPACE = "ViewModels";
         const string DEFAULT_V_NAMESPACE = "Views";
         const string DEFAULT_VM_SUFFIX = "ViewModel";
@@ -20,8 +24,27 @@ namespace XamarinFormsMvvmAdaptor
         static string _viewSubNamespace = DEFAULT_V_NAMESPACE;
         static string _viewModelSuffix = DEFAULT_VM_SUFFIX;
         static string _viewSuffix = DEFAULT_V_SUFFIX;
-        #endregion
 
+        /// <summary>
+        /// Customise the controller's expected naming convention
+        /// </summary>
+        /// <param name="viewModelSubNamespace"></param>
+        /// <param name="viewSubNamespace"></param>
+        /// <param name="viewModelSuffix"></param>
+        /// <param name="viewSuffix"></param>
+        public static void SetNamingConventions(
+            string viewModelSubNamespace = DEFAULT_VM_NAMESPACE
+            , string viewSubNamespace = DEFAULT_V_NAMESPACE
+            , string viewModelSuffix = DEFAULT_VM_SUFFIX
+            , string viewSuffix = DEFAULT_V_SUFFIX)
+        {
+            _viewModelSubNamespace = viewModelSubNamespace;
+            _viewSubNamespace = viewSubNamespace;
+            _viewModelSuffix = viewModelSuffix;
+            _viewSuffix = viewSuffix;
+        }
+        #endregion
+        #region PROPERTIES
         public IIoc Ioc { get; } = new Ioc();
 
         Page root;
@@ -131,30 +154,309 @@ namespace XamarinFormsMvvmAdaptor
                 }
             }
         }
+        #endregion
+        #region INITIALISATION
+
+
+        public Page Initialize<TViewModel>(bool mustWrapInNavigationPage = true) where TViewModel : class, IMvvmViewModelBase
+        {
+            var viewModel = ResolveViewModel<TViewModel>();
+            var page = CreatePageFor<TViewModel>();
+
+            BindViewModelToPage(page, viewModel);
+            WirePageEventsToViewModel(viewModel, page);
+
+            NavigationRoot = mustWrapInNavigationPage
+                ? new NavigationPage(page)
+                : page;
+
+            IsInitialized = true;
+
+
+            if (page is TabbedPage tabbedPage)
+            {
+                if (viewModel is IMvvmTabbedViewModelBase)
+                    WireTabbedPageEventsToViewModel(viewModel as IMvvmTabbedViewModelBase, page as TabbedPage);
+                //Look for multi attribute
+                var constructorsDecorated = viewModel.GetType()
+                    .GetConstructors()
+                    .Where(c => c.GetCustomAttribute<MultipleNavigationAttribute>() != null);
+
+                if (constructorsDecorated.Any())
+                {
+                    var multiNavigationAttribute = constructorsDecorated
+                        .First()
+                        .GetCustomAttribute<MultipleNavigationAttribute>();
+
+                    var multiNav = Ioc.Resolve(typeof(IMultiNavigation)) as IMultiNavigation;
+
+                    int i = 0;
+                    foreach (var item in multiNavigationAttribute.MvvmControllerKeys)
+                    {
+                        (multiNav.NavigationControllers[item] as Mvvm).InitializeTabMvvmController(tabbedPage.Children[i++]);
+                    }
+                    //foreach (var item in multi)
+                    //{
+                    //    if (item.Value.RootPage.GetType() == childPage.GetType())
+                    //        Console.WriteLine(childPage.GetType().Name);
+                    //}
+
+                }
+                else
+                {
+                    foreach (var childPage in tabbedPage.Children)
+                    {
+                        var childViewModel =
+                            childPage is NavigationPage
+                            ? CreateViewModelFor((childPage as NavigationPage).RootPage)
+                            : CreateViewModelFor(childPage);
+                        childPage.BindingContext = childViewModel;
+                        WirePageEventsToViewModel(childViewModel, childPage);
+                    }
+                }
+            }
+
+
+            return NavigationRoot;
+        }
+        private void InitializeTabMvvmController(Page tabChild)
+        {
+            var isNavigationPage = tabChild is NavigationPage;
+            Page page = isNavigationPage
+                ? (tabChild as NavigationPage).RootPage
+                : tabChild;
+
+            var viewModel = CreateViewModelFor(page);
+            BindViewModelToPage(page, viewModel);
+            WirePageEventsToViewModel(viewModel, page);
+
+            NavigationRoot = isNavigationPage
+                ? tabChild as NavigationPage
+                : new NavigationPage(page);
+
+            IsInitialized = true;
+        }
 
         private void ThrowIfNotInitialized()
         {
             if (!IsInitialized)
                 throw new NotInitializedException(
-                    $"{nameof(Mvvm)} is not initialized. Please run {nameof(InitAsync)} first.");
+                    $"{nameof(Mvvm)} is not initialized. Please run {nameof(Initialize)} first.");
         }
-
-        private async Task InitializeVmAsync(Page page, object initialisationParameter, bool continueOnCapturedContext = false)
+        #endregion
+        #region CONSTRUCTIVE
+        public Task<TViewModel> PushAsync<TViewModel>(TViewModel viewModel, object navigationData = null, bool animated = true)
+            where TViewModel : class, IMvvmViewModelBase
         {
-            try
-            {
-                await (page.BindingContext as IMvvmViewModelBase).OnViewPushedAsync(initialisationParameter).ConfigureAwait(continueOnCapturedContext);
-            }
-            catch (NullReferenceException ex)
-            {
-                throw new NullReferenceException("Check if your ViewModel is attached to the Page", ex);
-            }
-            catch (InvalidCastException ex)
-            {
-                throw new InvalidCastException($"Check if your ViewModel implements {nameof(IMvvmViewModelBase)}", ex);
-            }
+            return InternalPushAsync(viewModel, navigationData, animated);
         }
 
+        public Task<TViewModel> PushModalAsync<TViewModel>(TViewModel viewModel,
+            object navigationData = null, bool animated = true)
+            where TViewModel : class, IMvvmViewModelBase
+        {
+            return InternalPushAsync(viewModel, navigationData, animated, isModal: true);
+        }
+
+        ///<inheritdoc/>
+        public Task<TViewModel> PushAsync<TViewModel>(
+            object navigationData = null, bool animated = true)
+            where TViewModel : class, IMvvmViewModelBase
+        {
+            var viewModel = ResolveViewModel<TViewModel>();
+
+            return InternalPushAsync(viewModel, navigationData, animated);
+        }
+
+        ///<inheritdoc/>
+        public Task<TViewModel> PushModalAsync<TViewModel>(
+            object navigationData = null, bool animated = true)
+            where TViewModel : class, IMvvmViewModelBase
+        {
+            var viewModel = ResolveViewModel<TViewModel>();
+
+            return InternalPushAsync(viewModel, navigationData, animated, isModal: true);
+        }
+
+        async Task<TViewModel> InternalPushAsync<TViewModel>(TViewModel viewModel,
+            object navigationData = null, bool animated = true, bool isModal = false)
+            where TViewModel : class, IMvvmViewModelBase
+        {
+            //todo clean up unused InstantiatePage and Other such things once finished with NewStyle stuff
+            var page = CreatePageFor<TViewModel>();
+            BindViewModelToPage(page, viewModel);
+            WirePageEventsToViewModel(viewModel, page);
+
+            var isPushedTcs = new TaskCompletionSource<bool>();
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                try
+                {
+                    if (isModal)
+                        await NavigationRoot.Navigation.PushModalAsync(
+                            ModalStack.Any()
+                            ? page
+                            : new NavigationPage(page)
+                            , animated);
+                    else
+                        await NavigationRoot.Navigation.PushAsync(page, animated);
+
+                    isPushedTcs.SetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    isPushedTcs.SetException(ex);
+                }
+            });
+
+            if (await isPushedTcs.Task)
+                await viewModel.OnViewPushedAsync(navigationData).ConfigureAwait(false);
+
+            return viewModel as TViewModel;
+        }
+
+        private IMvvmViewModelBase CreateViewModelFor(Page child)
+        {
+            var viewModelType = GetViewModelTypeForPage(child.GetType());
+            return ResolveViewModel(viewModelType, mustTryInterfaceVariation: true);
+        }
+
+        private Page CreatePageFor<TViewModel>() where TViewModel : IMvvmViewModelBase
+        {
+            return Activator.CreateInstance(
+                    GetPageTypeForViewModel<TViewModel>()) as Page;
+        }
+
+        private void WirePageEventsToViewModel(IMvvmViewModelBase viewModel, Page page)
+        {
+            //todo consider just taking a page and assuming its wired-up (note assumption required)
+            page.Appearing += new WeakEventHandler<EventArgs>(
+                viewModel.OnViewAppearing).Handler;
+            page.Disappearing += new WeakEventHandler<EventArgs>(
+                viewModel.OnViewDisappearing).Handler;
+        }
+
+        private void WireTabbedPageEventsToViewModel(IMvvmTabbedViewModelBase viewModel, TabbedPage page)
+        {
+
+            page.CurrentPageChanged += new WeakEventHandler<EventArgs>(
+                viewModel.OnTabbedViewCurrentPageChanged).Handler;
+        }
+
+        private TViewModel ResolveViewModel<TViewModel>() where TViewModel : class, IMvvmViewModelBase
+        {
+            return ResolveViewModel(typeof(TViewModel)) as TViewModel;
+        }
+
+        private IMvvmViewModelBase ResolveViewModel(Type viewModelType, bool mustTryInterfaceVariation = false)
+        {
+            if (typeof(IMvvmViewModelBase).IsAssignableFrom(viewModelType.GetType()))
+                throw new InvalidOperationException("viewModelType is expected to implement IAdaptorViewModel");
+
+
+            if (mustTryInterfaceVariation)
+            {
+                var viewModelInterfaceTypeName = string.Format(CultureInfo.InvariantCulture
+                    , "{0}.{1}, {2}"
+                    , viewModelType.Namespace
+                    , $"I{viewModelType.Name}"
+                    , viewModelType.GetTypeInfo().Assembly.FullName);
+
+                var iviewModelType = Type.GetType(viewModelInterfaceTypeName);
+
+                if (iviewModelType != null
+                    && Ioc.IsRegistered(iviewModelType))
+                    return Ioc.Resolve(iviewModelType) as IMvvmViewModelBase;
+            }
+
+            //if ResolveMode not strict then will attempt Activator.Create
+            return Ioc.Resolve(viewModelType) as IMvvmViewModelBase;
+
+            throw new InvalidOperationException(
+                $"Could not Resolve {viewModelType.Name}" +
+                $". It is not registered " +
+                $"in {nameof(Mvvm)}.{nameof(Ioc)}.");
+        }
+
+        private Type GetPageTypeForViewModel<TViewModel>()
+        {
+            return GetPageTypeForViewModel(typeof(TViewModel));
+        }
+
+        private Type GetPageTypeForViewModel(Type viewModelType)
+        {
+            var nameSpace = viewModelType.Namespace
+                            .Replace(_viewModelSubNamespace, _viewSubNamespace);
+            var name =
+                viewModelType.IsInterface && viewModelType.Name.StartsWith("I")
+                ? ReplaceLastOccurrence(
+                            viewModelType.Name.Substring(1), _viewModelSuffix, _viewSuffix)
+                : ReplaceLastOccurrence(
+                            viewModelType.Name, _viewModelSuffix, _viewSuffix);
+
+            var viewAssemblyName = string.Format(CultureInfo.InvariantCulture
+                , "{0}.{1}, {2}"
+                , nameSpace
+                , name
+                , viewModelType.GetTypeInfo().Assembly.FullName);
+
+            return Type.GetType(viewAssemblyName);
+        }
+
+        private Type GetViewModelTypeForPage(Type pageType)
+        {
+            var nameSpace = pageType.Namespace
+                            .Replace(_viewSubNamespace, _viewModelSubNamespace);
+            var name = ReplaceLastOccurrence(
+                            pageType.Name, _viewSuffix, _viewModelSuffix);
+
+            var viewAssemblyName = string.Format(CultureInfo.InvariantCulture
+                , "{0}.{1}, {2}"
+                , nameSpace
+                , name
+                , pageType.GetTypeInfo().Assembly.FullName);
+
+            return Type.GetType(viewAssemblyName);
+        }
+
+        private void BindViewModelToPage(Page page, IMvvmViewModelBase viewModel)
+        {
+            page.GetType().GetProperty("BindingContext").SetValue(page, viewModel);
+        }
+
+        #endregion
+
+        #region HELPERS
+
+        private string ReplaceLastOccurrence(string source, string find, string replace)
+        {
+            int place = source.LastIndexOf(find);
+
+            if (place == -1)
+                return source;
+
+            return source.Remove(place, find.Length).Insert(place, replace);
+        }
+
+        #endregion
+
+
+        //private async Task InitializeVmAsync(Page page, object initialisationParameter, bool continueOnCapturedContext = false)
+        //{
+        //    try
+        //    {
+        //        await (page.BindingContext as IMvvmViewModelBase).OnViewPushedAsync(initialisationParameter).ConfigureAwait(continueOnCapturedContext);
+        //    }
+        //    catch (NullReferenceException ex)
+        //    {
+        //        throw new NullReferenceException("Check if your ViewModel is attached to the Page", ex);
+        //    }
+        //    catch (InvalidCastException ex)
+        //    {
+        //        throw new InvalidCastException($"Check if your ViewModel implements {nameof(IMvvmViewModelBase)}", ex);
+        //    }
+        //}
+        #region DESTRUCTIVE
         ///<inheritdoc/>
         public async Task RemovePreviousPageFromMainStack()
         {
@@ -232,7 +534,7 @@ namespace XamarinFormsMvvmAdaptor
         }
 
         ///<inheritdoc/>
-        public async Task<IMvvmViewModelBase> PopToRootAsync(bool animated = true)
+        public async Task<IMvvmViewModelBase> PopMainStackToRootAsync(bool animated = true)
         {
             var poppedViewModel = MainStack.GetCurrentViewModel();
 
@@ -288,5 +590,6 @@ namespace XamarinFormsMvvmAdaptor
 
             return TopViewModel;
         }
+        #endregion
     }
 }
