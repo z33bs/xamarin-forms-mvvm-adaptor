320636163811771392ec7dbbfc32f369514b8faf
Guy Antoine <guy@zeebz.net>
2020-04-25 06:21:48 +0200

    Import Xamarin.Forms.Command

--- XamarinFormsMvvmAdaptor.Tests/CommandTests.cs
+++ XamarinFormsMvvmAdaptor.Tests/CommandTests.cs
@@ -1,53 +1,53 @@
 ﻿using System;
 using Xunit;
-using XamarinFormsMvvmAdaptor;
+using XamarinFormsMvvmAdaptor.Helpers;
 
 namespace XamarinFormsMvvmAdaptor.Tests
 {
-	public class BackgroundCommandTests
+	public class CommandTests
 	{
 		[Fact]
 		public void Constructor()
 		{
-			var cmd = new BackgroundCommand(() => { });
+			var cmd = new Command(() => { });
 			Assert.True(cmd.CanExecute(null));
 		}
 
 		[Fact]
 		public void ThrowsWithNullConstructor()
 		{
-			Assert.Throws<ArgumentNullException>(() => new BackgroundCommand((Action)null));
+			Assert.Throws<ArgumentNullException>(() => new Command((Action)null));
 		}
 
 		[Fact]
 		public void ThrowsWithNullParameterizedConstructor()
 		{
-			Assert.Throws<ArgumentNullException>(() => new BackgroundCommand((Action<object>)null));
+			Assert.Throws<ArgumentNullException>(() => new Command((Action<object>)null));
 		}
 
 		[Fact]
 		public void ThrowsWithNullCanExecute()
 		{
-			Assert.Throws<ArgumentNullException>(() => new BackgroundCommand(() => { }, null));
+			Assert.Throws<ArgumentNullException>(() => new Command(() => { }, null));
 		}
 
 		[Fact]
 		public void ThrowsWithNullParameterizedCanExecute()
 		{
-			Assert.Throws<ArgumentNullException>(() => new BackgroundCommand(o => { }, null));
+			Assert.Throws<ArgumentNullException>(() => new Command(o => { }, null));
 		}
 
 		[Fact]
 		public void ThrowsWithNullExecuteValidCanExecute()
 		{
-			Assert.Throws<ArgumentNullException>(() => new BackgroundCommand(null, () => true));
+			Assert.Throws<ArgumentNullException>(() => new Command(null, () => true));
 		}
 
 		[Fact]
 		public void Execute()
 		{
 			bool executed = false;
-			var cmd = new BackgroundCommand(() => executed = true);
+			var cmd = new Command(() => executed = true);
 
 			cmd.Execute(null);
 			Assert.True(executed);
@@ -57,7 +57,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		public void ExecuteParameterized()
 		{
 			object executed = null;
-			var cmd = new BackgroundCommand(o => executed = o);
+			var cmd = new Command(o => executed = o);
 
 			var expected = new object();
 			cmd.Execute(expected);
@@ -69,7 +69,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		public void ExecuteWithCanExecute()
 		{
 			bool executed = false;
-			var cmd = new BackgroundCommand(() => executed = true, () => true);
+			var cmd = new Command(() => executed = true, () => true);
 
 			cmd.Execute(null);
 			Assert.True(executed);
@@ -81,7 +81,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		public void CanExecute(bool expected)
 		{
 			bool canExecuteRan = false;
-			var cmd = new BackgroundCommand(() => { }, () => {
+			var cmd = new Command(() => { }, () => {
 				canExecuteRan = true;
 				return expected;
 			});
@@ -94,7 +94,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		public void ChangeCanExecute()
 		{
 			bool signaled = false;
-			var cmd = new BackgroundCommand(() => { });
+			var cmd = new Command(() => { });
 
 			cmd.CanExecuteChanged += (sender, args) => signaled = true;
 
@@ -105,26 +105,26 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		[Fact]
 		public void GenericThrowsWithNullExecute()
 		{
-			Assert.Throws<ArgumentNullException>(() => new BackgroundCommand<string>(null));
+			Assert.Throws<ArgumentNullException>(() => new Command<string>(null));
 		}
 
 		[Fact]
 		public void GenericThrowsWithNullExecuteAndCanExecuteValid()
 		{
-			Assert.Throws<ArgumentNullException>(() => new BackgroundCommand<string>(null, s => true));
+			Assert.Throws<ArgumentNullException>(() => new Command<string>(null, s => true));
 		}
 
 		[Fact]
 		public void GenericThrowsWithValidExecuteAndCanExecuteNull()
 		{
-			Assert.Throws<ArgumentNullException>(() => new BackgroundCommand<string>(s => { }, null));
+			Assert.Throws<ArgumentNullException>(() => new Command<string>(s => { }, null));
 		}
 
 		[Fact]
 		public void GenericExecute()
 		{
 			string result = null;
-			var cmd = new BackgroundCommand<string>(s => result = s);
+			var cmd = new Command<string>(s => result = s);
 
 			cmd.Execute("Foo");
 			Assert.Equal("Foo", result);
@@ -134,7 +134,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		public void GenericExecuteWithCanExecute()
 		{
 			string result = null;
-			var cmd = new BackgroundCommand<string>(s => result = s, s => true);
+			var cmd = new Command<string>(s => result = s, s => true);
 
 			cmd.Execute("Foo");
 			Assert.Equal("Foo", result);
@@ -146,7 +146,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		public void GenericCanExecute(bool expected)
 		{
 			string result = null;
-			var cmd = new BackgroundCommand<string>(s => { }, s => {
+			var cmd = new Command<string>(s => { }, s => {
 				result = s;
 				return expected;
 			});
@@ -167,7 +167,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		[Fact]
 		public void CanExecuteReturnsFalseIfParameterIsWrongReferenceType()
 		{
-			var command = new BackgroundCommand<FakeChildContext>(context => { }, context => true);
+			var command = new Command<FakeChildContext>(context => { }, context => true);
 
 			Assert.False(command.CanExecute(new FakeParentContext()), "the parameter is of the wrong type");
 		}
@@ -175,7 +175,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		[Fact]
 		public void CanExecuteReturnsFalseIfParameterIsWrongValueType()
 		{
-			var command = new BackgroundCommand<int>(context => { }, context => true);
+			var command = new Command<int>(context => { }, context => true);
 
 			Assert.False(command.CanExecute(10.5), "the parameter is of the wrong type");
 		}
@@ -183,7 +183,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		[Fact]
 		public void CanExecuteUsesParameterIfReferenceTypeAndSetToNull()
 		{
-			var command = new BackgroundCommand<FakeChildContext>(context => { }, context => true);
+			var command = new Command<FakeChildContext>(context => { }, context => true);
 
 			Assert.True(command.CanExecute(null), "null is a valid value for a reference type");
 		}
@@ -191,7 +191,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		[Fact]
 		public void CanExecuteUsesParameterIfNullableAndSetToNull()
 		{
-			var command = new BackgroundCommand<int?>(context => { }, context => true);
+			var command = new Command<int?>(context => { }, context => true);
 
 			Assert.True(command.CanExecute(null), "null is a valid value for a Nullable<int> type");
 		}
@@ -199,7 +199,7 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		[Fact]
 		public void CanExecuteIgnoresParameterIfValueTypeAndSetToNull()
 		{
-			var command = new BackgroundCommand<int>(context => { }, context => true);
+			var command = new Command<int>(context => { }, context => true);
 
 			Assert.False(command.CanExecute(null), "null is not a valid valid for int");
 		}
@@ -208,61 +208,61 @@ namespace XamarinFormsMvvmAdaptor.Tests
 		public void ExecuteDoesNotRunIfParameterIsWrongReferenceType()
 		{
 			int executions = 0;
-			var BackgroundCommand = new BackgroundCommand<FakeChildContext>(context => executions += 1);
+			var Command = new Command<FakeChildContext>(context => executions += 1);
 
-//			Assert.DoesNotThrow(() => BackgroundCommand.Execute(new FakeParentContext()), "the BackgroundCommand should not execute, so no exception should be thrown");
-			var exception = Record.Exception(() => BackgroundCommand.Execute(new FakeParentContext()));
+//			Assert.DoesNotThrow(() => Command.Execute(new FakeParentContext()), "the Command should not execute, so no exception should be thrown");
+			var exception = Record.Exception(() => Command.Execute(new FakeParentContext()));
 			Assert.Null(exception);
 
-			Assert.True(executions == 0, "the BackgroundCommand should not have executed");
+			Assert.True(executions == 0, "the Command should not have executed");
 		}
 
 		[Fact]
 		public void ExecuteDoesNotRunIfParameterIsWrongValueType()
 		{
 			int executions = 0;
-			var BackgroundCommand = new BackgroundCommand<int>(context => executions += 1);
+			var Command = new Command<int>(context => executions += 1);
 
-//			Assert.DoesNotThrow(() => BackgroundCommand.Execute(10.5), "the BackgroundCommand should not execute, so no exception should be thrown");
-			var exception = Record.Exception(() => BackgroundCommand.Execute(10.5));
+//			Assert.DoesNotThrow(() => Command.Execute(10.5), "the Command should not execute, so no exception should be thrown");
+			var exception = Record.Exception(() => Command.Execute(10.5));
 			Assert.Null(exception);
 
-			Assert.True(executions == 0, "the BackgroundCommand should not have executed");
+			Assert.True(executions == 0, "the Command should not have executed");
 		}
 
 		[Fact]
 		public void ExecuteRunsIfReferenceTypeAndSetToNull()
 		{
 			int executions = 0;
-			var BackgroundCommand = new BackgroundCommand<FakeChildContext>(context => executions += 1);
+			var Command = new Command<FakeChildContext>(context => executions += 1);
 
-			var exception = Record.Exception(() => BackgroundCommand.Execute(null));
+			var exception = Record.Exception(() => Command.Execute(null));
 			Assert.Null(exception);
 			//"null is a valid value for a reference type");
-			Assert.True(executions == 1, "the BackgroundCommand should have executed");
+			Assert.True(executions == 1, "the Command should have executed");
 		}
 
 		[Fact]
 		public void ExecuteRunsIfNullableAndSetToNull()
 		{
 			int executions = 0;
-			var BackgroundCommand = new BackgroundCommand<int?>(context => executions += 1);
+			var Command = new Command<int?>(context => executions += 1);
 
-			var exception = Record.Exception(() => BackgroundCommand.Execute(null));
+			var exception = Record.Exception(() => Command.Execute(null));
 			//"null is a valid value for a Nullable<int> type");
 			Assert.Null(exception);
-			Assert.True(executions == 1, "the BackgroundCommand should have executed");
+			Assert.True(executions == 1, "the Command should have executed");
 		}
 
 		[Fact]
 		public void ExecuteDoesNotRunIfValueTypeAndSetToNull()
 		{
 			int executions = 0;
-			var BackgroundCommand = new BackgroundCommand<int>(context => executions += 1);
+			var Command = new Command<int>(context => executions += 1);
 
-			var exception = Record.Exception(() => BackgroundCommand.Execute(null));
+			var exception = Record.Exception(() => Command.Execute(null));
 			Assert.Null(exception);
-			Assert.True(executions == 0, "the BackgroundCommand should not have executed");
+			Assert.True(executions == 0, "the Command should not have executed");
 		}
 	}
 }
