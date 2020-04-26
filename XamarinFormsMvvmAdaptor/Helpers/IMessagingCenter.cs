382817392c8779991ae7527033732a915cb3c5fc
Guy Antoine <guy@zeebz.net>
2020-04-26 08:14:42 +0200

    Add IMessagingCenter interface

---
+++ XamarinFormsMvvmAdaptor/Helpers/IMessagingCenter.cs
@@ -0,0 +1,42 @@
+﻿//From Xamarin.Forms https://github.com/xamarin/Xamarin.Forms
+using System;
+namespace XamarinFormsMvvmAdaptor.Helpers
+{
+	/// <summary>
+	/// Interface for <see cref="Xamarin.Forms.MessagingCenter"/> to avoid
+    /// unnecessary `using Xamarin.Forms;` reference in your ViewModels
+    /// Associates a callback on subscribers with a specific message name
+	/// </summary>
+	public interface IMessagingCenter
+	{
+		/// <summary>
+		/// Sends a named message with the specified arguments
+		/// </summary>
+		void Send<TSender, TArgs>(TSender sender, string message, TArgs args) where TSender : class;
+
+		/// <summary>
+		/// Sends a named message that has no arguments
+		/// </summary>
+		void Send<TSender>(TSender sender, string message) where TSender : class;
+
+		/// <summary>
+		/// Run the callback on subscriber in response to parameterised messages that are named message and that are created by source.
+		/// </summary>
+		void Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source = null) where TSender : class;
+
+		/// <summary>
+		/// Run the callback on subscriber in response to messages that are named message and that are created by source.
+		/// </summary>
+		void Subscribe<TSender>(object subscriber, string message, Action<TSender> callback, TSender source = null) where TSender : class;
+
+		/// <summary>
+		/// Unsubscribes from the specified parameterless subscriber messages
+		/// </summary>
+		void Unsubscribe<TSender, TArgs>(object subscriber, string message) where TSender : class;
+
+		/// <summary>
+		/// Unsubscribes a subscriber from the specified messages that come from the specified sender
+		/// </summary>
+		void Unsubscribe<TSender>(object subscriber, string message) where TSender : class;
+	}
+}
